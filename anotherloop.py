eb9cc2bd87fcc8e472287befbad4461fa5261893
taha <157030069+NOTBAITED@users.noreply.github.com>
2024-02-26 00:11:09 +0100

    Add files via upload

---
+++ anotherloop.py
@@ -0,0 +1,7 @@
+age = int(input("please enter your age "))
+
+while age < 20:
+    print("not old enough")
+    age = int(input("please enter your age "))
+else:
+    print("nice")
---
+++ anotherloop2.py
@@ -0,0 +1,7 @@
+name = (input("please enter your name "))
+
+while name != ("BAITED"): 
+    print("wrong name")
+    name = (input("please enter your name "))
+else: name == ("BAITED")
+print("welcomeback")
